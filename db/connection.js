3ea008a80d9b69dbe03ee8453e52ea81fc7f87a2
gurungpratik <pratikgurung365@gmail.com>
2022-12-15 16:13:17 +0000

    hosting updates

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,17 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+const config =
+	ENV === 'production'
+		? {
+				connectionString: process.env.DATABASE_URL,
+				max: 2,
+		  }
+		: {};
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
